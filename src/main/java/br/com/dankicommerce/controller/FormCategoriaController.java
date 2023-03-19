8c14dab467913a77a6a777ab696935c21091151a
rafaelspa <rsdepaula67@gmail.com>
2023-03-19 08:32:01 -0300

    Controllers e paginas

---
+++ src/main/java/br/com/dankicommerce/controller/FormCategoriaController.java
@@ -0,0 +1,14 @@
+package br.com.dankicommerce.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+@Path("formcategoria")
+public class FormCategoriaController {
+
+	@Get("")
+	public void formcategoria() {
+	}
+}
