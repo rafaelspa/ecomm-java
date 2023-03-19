6ca64fa367135e176253a83c7a59949fb77ec3ef
rafaelspa <rsdepaula67@gmail.com>
2023-03-19 09:45:50 -0300

    salvaUsuario + cadastrar.jsp form

--- src/main/java/br/com/dankicommerce/controller/CadastrarController.java
+++ src/main/java/br/com/dankicommerce/controller/CadastrarController.java
@@ -3,6 +3,8 @@ package br.com.dankicommerce.controller;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.dankicommerce.model.Usuario;
 
 @Controller
 @Path("cadastrar")
@@ -10,5 +12,10 @@ public class CadastrarController {
 
 	@Get("")
 	public void cadastrar() {
+	}
+	
+	@Post("salvaUsuario")
+	public void salvaUsuario(Usuario usuario) {
+		
 	}
 }
