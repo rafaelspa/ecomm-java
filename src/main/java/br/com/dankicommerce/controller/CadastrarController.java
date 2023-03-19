33fa868f7be994a2586fe74f896c2dbc39d407f0
rafaelspa <rsdepaula67@gmail.com>
2023-03-18 21:34:47 -0300

    Start, login, cadastrar, categorias

--- src/main/java/br/com/dankicommerce/controller/CadastrarController.java
+++ src/main/java/br/com/dankicommerce/controller/CadastrarController.java
@@ -5,10 +5,10 @@ import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 
 @Controller
-@Path("login")
-public class LoginController {
+@Path("cadastrar")
+public class CadastrarController {
 
 	@Get("")
-	public void login() {
+	public void cadastrar() {
 	}
 }
